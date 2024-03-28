8918c83f73bd445f92679c99ca03a9894e426de0
Valentyn-Melnyk <v.iv.melnyk@gmail.com>
2024-03-28 18:54:24 +0200

    HW-JS-08

--- js/task-1.js
+++
@@ -1,7 +0,0 @@
-const categories = document.querySelectorAll('.item');
-console.log(`Number of categories: ${categories.length}`);
-
-categories.forEach((category, index) => {
-  console.log(`Category: ${category.firstElementChild.textContent}`);
-  console.log(`Elements: ${category.lastElementChild.children.length}`);
-});
